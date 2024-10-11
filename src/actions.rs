b61682318e0c9eb8bc5305106f57fdef1bd6e0f2
Wolfyxon <wolfyxon@gmail.com>
2024-10-11 20:43:22 +0200

    execute() now shows the output of the executed command

--- src/actions.rs
+++ src/actions.rs
@@ -1,32 +1,31 @@
 use std::process::Command;
 use std::process::exit;
-use std::process::Output;
 use ansi_term::Style;
 use ansi_term::Color::Blue;
 use crate::output::print_err;
 
-pub fn execute(command: &str, args: Vec<String>) -> Output {
+pub fn execute(command: &str, args: Vec<String>) -> std::process::ExitStatus {
     let prefix = Style::new().fg(Blue).paint("Running >");
     println!("{} {} {}", prefix, command, args.join(" "));
 
     let res = Command::new(command)
         .args(args)
-        .output();
+        .status();
     
     if res.is_err() {
         print_err("Failed to execute command".to_string());    
         exit(1);
     }
 
-    let out = res.unwrap();
+    let status: std::process::ExitStatus = res.unwrap();
     
     println!("");
-    println!("Command exited with code: {}", out.status.code().unwrap());
+    println!("Command exited with code: {}", status.code().unwrap());
 
-    if !out.status.success() {
+    if !status.success() {
         print_err("Command failed".to_string());
         exit(1);
     }
 
-    out
+    status
 }
