b3d118c89e8cb4a823c7754c56a17b1cfd494a96
Wolfyxon <wolfyxon@gmail.com>
2024-10-12 11:27:23 +0200

    Success message is printed when quiet mode is disabled

--- src/actions.rs
+++ src/actions.rs
@@ -50,10 +50,12 @@ pub fn execute(command: &str, args: Vec<String>, quiet: bool) -> std::process::E
 
     let status: std::process::ExitStatus = res.unwrap();
     let exit_code = status.code().unwrap();
-    
+
     if status.success() {
-        println!("");
-        println!("Command exited with code: {}", exit_code);
+        if !quiet {
+            println!("");
+            println!("Command exited with code: {}", exit_code);
+        }
     } else {
         print_err(format!("Command failed with code: {}", exit_code));
         exit(1);
