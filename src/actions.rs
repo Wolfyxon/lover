d7495a597aab3dca6e1d951980d7b38af22a358b
Wolfyxon <wolfyxon@gmail.com>
2024-10-12 11:32:01 +0200

    Added get_file_tree_of_type()

--- src/actions.rs
+++ src/actions.rs
@@ -83,3 +83,22 @@ pub fn get_file_tree(root: &Path) -> Result<Vec<PathBuf>, std::io::Error> {
 
     Ok(res)
 }
+
+pub fn get_file_tree_of_type(root: &Path, extension: &str) -> Result<Vec<PathBuf>, std::io::Error> {
+    let tree = get_file_tree(root)?;
+    let mut res: Vec<PathBuf> = Vec::new();
+
+    for path in tree {
+        let ext_res = path.extension();
+
+        if ext_res.is_none() {
+            continue;
+        }
+        
+        if ext_res.unwrap() == extension {
+            res.push(path);
+        }
+    }
+
+    Ok(res)
+}
