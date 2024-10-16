3d5a80dbadb6ad90a1edbb0d50363470cc090601
Wolfyxon <wolfyxon@gmail.com>
2024-10-16 22:51:10 +0200

    Added builder function

--- src/targets.rs
+++ src/targets.rs
@@ -1,10 +1,13 @@
+use std::path::Path;
 use crate::actions;
+use crate::project_config;
 
 pub struct BuildTarget<'a> {
     pub name: &'a str,
     pub description: &'a str,
     pub deps: Vec<&'a str>,
-    pub previous: Vec<&'a str>
+    pub previous: Vec<&'a str>,
+    builder: fn()
 }
 
 pub fn get_targets<'a>() -> Vec<BuildTarget<'a>> {
@@ -13,7 +16,14 @@ pub fn get_targets<'a>() -> Vec<BuildTarget<'a>> {
             name: "love",
             description: "Game's code packaged in the Love format.",
             deps: Vec::new(),
-            previous: Vec::new()
+            previous: Vec::new(),
+            builder: || {
+                let config = project_config::get();
+                let output = Path::new(config.directories.build.as_str()).join(config.package.name + ".love");
+            
+                actions::parse_all(Path::new(&project_config::get().directories.source));
+                actions::archive(Path::new(config.directories.source.as_str()), &output);
+            }
         }
     ]
 }
