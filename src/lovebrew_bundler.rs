f8e34f89cc9076171adce089d5b0c4cb8a8b0cfb
Wolfyxon <wolfyxon@gmail.com>
2024-11-08 17:22:15 +0100

    Init bundler data structs

---
+++ src/lovebrew_bundler.rs
@@ -0,0 +1,24 @@
+use serde::Serialize;
+
+// https://lovebrew.org/bundler/getting-started/configuration
+
+#[derive(Serialize)]
+pub struct BundlerConfig {
+    pub metadata: Metadata,
+    pub build: Build
+}
+
+#[derive(Serialize)]
+pub struct Metadata {
+    pub title: String,
+    pub author: String,
+    pub description: String,
+    pub version: String
+}
+
+#[derive(Serialize)]
+pub struct Build {
+    pub targets: Vec<String>,
+    pub source: String,
+    pub packaged: bool
+}
