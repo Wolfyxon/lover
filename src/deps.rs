05a2da9adb9962c716d332ea785b233b1f540a1b
Wolfyxon <wolfyxon@gmail.com>
2024-11-09 11:29:09 +0100

    Commented unused RepoDownload

--- src/deps.rs
+++ src/deps.rs
@@ -4,10 +4,11 @@ use serde::Deserialize;
 
 use crate::{config, console::{confirm_or_exit, exit_err, print_stage, print_success, ProgressBar}, http};
 
+/*
 pub enum RepoDownload {
     LatestRelease,
     Source
-}
+}*/
 
 #[derive(Deserialize)]
 pub struct GitHubRelease { 
