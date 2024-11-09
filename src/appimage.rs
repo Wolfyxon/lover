3864bbafdb3830bb43a9e7d09ebf4ca0ea44a6b6
Wolfyxon <wolfyxon@gmail.com>
2024-11-09 13:16:04 +0100

    Removed unused imports

--- src/appimage.rs
+++ src/appimage.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom, Write};
-use backhand::{FilesystemReader, InnerNode, SquashfsFileReader, SquashfsReadFile};
+use backhand::{FilesystemReader, InnerNode};
 
 use crate::console::{exit_err, print_warn};
 use crate::files;
