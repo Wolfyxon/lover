40be940ff56e605fd3e65ea949a1e44f6613654c
Wolfyxon <wolfyxon@gmail.com>
2025-03-23 14:58:46 +0100

    Better template

--- src/template/src/main.lua
+++ src/template/src/main.lua
@@ -1,5 +1,10 @@
-local text = love.graphics.newText(love.graphics.getFont(), "Hello World!")
+local text = "Hello World!"
+local textObj = love.graphics.newText(love.graphics.getFont(), text)
+
+local version = os.getenv("LOVER_PKG_VERSION")
+local gameName = os.getenv("LOVER_PKG_NAME")
 
 function love.draw()
-    love.graphics.draw(text)
+    print(gameName, "v.", version)
+    love.graphics.draw(textObj)
 end
