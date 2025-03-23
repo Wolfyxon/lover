9df3d4f9fb8b35d114d1bd91d324e2a3d2a7b2b8
Wolfyxon <wolfyxon@gmail.com>
2025-03-23 14:59:07 +0100

    Fixed misplaced print

--- src/template/src/main.lua
+++ src/template/src/main.lua
@@ -4,7 +4,8 @@ local textObj = love.graphics.newText(love.graphics.getFont(), text)
 local version = os.getenv("LOVER_PKG_VERSION")
 local gameName = os.getenv("LOVER_PKG_NAME")
 
+print(gameName, "v.", version)
+
 function love.draw()
-    print(gameName, "v.", version)
     love.graphics.draw(textObj)
 end
