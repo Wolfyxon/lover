213e1c92755ceff1bd225fb9aac7e1c42a30c74f
Wolfyxon <wolfyxon@gmail.com>
2025-06-16 14:27:45 +0200

    Skipping root

--- src/meta.rs
+++ src/meta.rs
@@ -2,6 +2,8 @@ use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
 
+use crate::files;
+
 #[derive(Serialize, Deserialize)]
 pub struct ProjectMeta {
     pub files: Vec<FileEntry>
@@ -20,13 +22,13 @@ pub struct FileEntry {
 }
 
 impl FileEntry {
-    pub fn new(path: impl Into<PathBuf>) -> Result<Self, String> {
+    pub fn new(path: impl Into<PathBuf>, root: impl Into<PathBuf>) -> Result<Self, String> {
         let path = path.into();
         let hash = sha256::try_digest(&path);
         
         match hash {
             Ok(hash) => Ok(Self {
-                path: path,
+                path: files::skip_path(path, root),
                 hash: hash
             }),
 
