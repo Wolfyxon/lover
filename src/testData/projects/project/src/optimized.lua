9bead48cd90d10bb540cb5a592fb22884c0b1ccf
Wolfyxon <wolfyxon@gmail.com>
2025-06-17 09:59:03 +0200

    Optimization test data for future

---
+++ src/testData/projects/project/src/optimized.lua
@@ -0,0 +1,8 @@
+
+print("Hi") 
+
+local flag = "--help"
+local flags = [[
+    --help: Shows help
+    --idk: Does something probably
+]]
---
+++ src/testData/projects/project/src/unoptimized.lua
@@ -0,0 +1,11 @@
+--[[
+    This contains a bunch of useless comments
+]]
+
+print("Hi") -- this prints "Hi" on the screen
+
+local flag = "--help"
+local flags = [[
+    --help: Shows help
+    --idk: Does something probably
+]]
